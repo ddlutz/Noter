49bc54577e6eb5f5768c05dfb4509c8ebc36f637
Douglas Dlutz <djd3681@rit.edu>
2012-10-23 09:41:41 -0400

    Initial Fragment setup
    Added a main activity to hold the two fragments, the two listfragments,
    and sample data.

---
+++ src/MainActivity.java
@@ -0,0 +1,13 @@
+import info.majesticcomputing.noter.R;
+import android.os.*;
+import android.support.v4.app.*;
+
+public class MainActivity extends FragmentActivity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.courses_lectures);
+	}
+
+}
